bb538344b9cba8a7408be5a3589c6d39e9279185
Mehmet  Ali Yücel <myuecel3@pz-koch19.uni-muenster.de>
2026-01-16 14:55:56 +0100

    Fix importing issues

--- stats_for_mols/visualization/__init__.py
+++ stats_for_mols/visualization/__init__.py
@@ -7,12 +7,15 @@ from .plots import plot_mcsim, plot_simultaneous_ci
 from .metrics_plot import MetricVisualizer
 
 # Diagnostic Plots (Data Split Quality)
-from .diagnostics import plot_target_distribution, plot_chemical_space_overlap
+from .diagnostics import plot_target_distribution, plot_chemical_space_overlap, plot_pca_chemical_space, plot_descriptor_overlap, plot_multiclass_target_distribution
 
 __all__ = [
     "plot_mcsim",
     "plot_simultaneous_ci",
     "MetricVisualizer",
     "plot_target_distribution",
-    "plot_chemical_space_overlap"
+    "plot_chemical_space_overlap",
+    "plot_pca_chemical_space",
+    "plot_descriptor_overlap",
+    "plot_multiclass_target_distribution",
 ]
